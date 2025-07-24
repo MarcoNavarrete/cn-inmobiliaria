74216d1e7dfd7896af4ea35dfe1bf0f779c81b21
MarcoNavarrete <camaleon0manc@gmail.com>
2025-07-23 22:42:38 -0600

    Corrige navegación usando Link en Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
 // src/components/Header.js
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <header className="header">
       <div className="logo">CN</div>
       <nav className='nav'>
-        <a href="/">Inicio</a>
+        {/* <a href="/">Inicio</a>
         <a href="/propiedades">Propiedades</a>
         <a href="/nosotros">Nosotros</a>
-        <a href="/contacto">Contacto</a>
+        <a href="/contacto">Contacto</a> */}
+        <Link to="/">Inicio</Link>
+        <Link to="/propiedades">Propiedades</Link>
+        <Link to="/nosotros">Nosotros</Link>
+        <Link to="/contacto">Contacto</Link>
       </nav>
     </header>
   );
