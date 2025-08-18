55f13f46c5bca95ad508678cb3ba6a0fddf443f5
MarcoNavarrete <camaleon0manc@gmail.com>
2025-08-18 16:29:30 -0600

    correción en el titulo

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -15,8 +15,8 @@ export default function Contacto() {
         </div>
         <div className="mapa">
           <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3746.18688934441!2d-98.74338322476767!3d20.12624928130677!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjDCsDA3JzM0LjUiTiA5OMKwNDQnMjYuOSJX!5e0!3m2!1ses!2smx!4v1753320075276!5m2!1ses!2smx" 
-          width="600" 
-          height="450" 
+          width="100%" 
+          height="450"
           style={{ border: 0 }} 
           allowfullscreen="" 
           loading="lazy" 
